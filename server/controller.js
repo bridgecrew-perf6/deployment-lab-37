3e15b99efcce97ecc5960e9f94293c40e1da88e0
Travis Gallagher <92351358+travisgallagher@users.noreply.github.com>
2022-03-16 15:01:52 -0600

    added controller and erased html to start over

---
+++ server/controller.js
@@ -0,0 +1,9 @@
+ export const func = () => {
+    console.log("first")
+}
+
+export const func2 = () => {
+    console.log("firstsss")
+}
+
+
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const controller = require("controller.js")
 
 const app = express(); 
 
