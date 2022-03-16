39a27c12018621a8034927c1285f194ac4f9ada8
Travis Gallagher <92351358+travisgallagher@users.noreply.github.com>
2022-03-16 14:29:25 -0600

    uncommented my important code, im an idiot

--- server/server.js
+++ server/server.js
@@ -7,8 +7,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// app.use(express.static(path.join(__dirname, "../public"))); 
-// app.use("/images", express.static(path.join(__dirname, "../images")))
+app.use(express.static(path.join(__dirname, "../public"))); 
+
 
 const port = process.env.PORT || 4005; 
 
